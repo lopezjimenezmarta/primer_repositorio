4d9cab73f5d2ae6face7029c7bd4daf23d3dd179
lopezjimenezmarta <lopezjimenezmarta13@gmail.com>
2014-09-08 19:52:22 +0200

    Primer ejercicio de JS

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+/* Comentario abnwnm mNKD 
+abnwnm mNKD,abnwnm mNKD,
+abnwnm mNKD, abnwnm mNKD,
+mnasmjnf nmeiuhr de Bloque*/
+
+// Comentario de Línea
+
+// Prueba de funcionamiento en JS
+//document.write("Si funciona mi JavaScript");
+
+// Generar alerta a usuario
+//alert("Ventana de alerta para usuario");
+
+//Solicitar información a nuestro usuario
+/*
+var nombre = prompt("Por favor, introduzca su nombre");
+document.write("<h1>Bienvenid@: " + nombre + "</h1><br />");
+
+var edad = prompt("Por favor, introduzca su edad");
+document.write("Su edad es: " + edad + "años");*/
+
+// Operaciones matemáticas 
+var numUno = 5;
+var numDos = 2;
+var numTres = 2;
+
+var suma = numUno + numDos;
+
+var resultado = suma / numTres;
+
+console.log(suma);
+
+console.log(resultado);
+
+// Operaciones matemáticas personalizadas
+var numUno = parseInt(prompt("Introduzca Primer valor"));
+var numDos = parseInt(prompt("Introduzca Segundo valor"));
+var numTres = parseInt(prompt("Introduzca Tercer valor"));
+
+var resultado = (numUno + numDos) / numTres;
+
+console.log("La operación es la siguiente: " + numUno + " + " + numDos + " / " + numTres);
+console.log(resultado);
