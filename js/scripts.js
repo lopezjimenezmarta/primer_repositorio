50a10bb7473c79979f18a2319862f2e91ffa0953
lopezjimenezmarta <lopezjimenezmarta13@gmail.com>
2014-09-09 19:58:38 +0200

    Variables
    Practicas con variables

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,7 @@ var edad = prompt("Por favor, introduzca su edad");
 document.write("Su edad es: " + edad + "años");*/
 
 // Operaciones matemáticas 
+/*
 var numUno = 5;
 var numDos = 2;
 var numTres = 2;
@@ -30,9 +31,10 @@ var resultado = suma / numTres;
 
 console.log(suma);
 
-console.log(resultado);
+console.log(resultado);*/
 
 // Operaciones matemáticas personalizadas
+/*
 var numUno = parseInt(prompt("Introduzca Primer valor"));
 var numDos = parseInt(prompt("Introduzca Segundo valor"));
 var numTres = parseInt(prompt("Introduzca Tercer valor"));
@@ -40,4 +42,46 @@ var numTres = parseInt(prompt("Introduzca Tercer valor"));
 var resultado = (numUno + numDos) / numTres;
 
 console.log("La operación es la siguiente: " + numUno + " + " + numDos + " / " + numTres);
-console.log(resultado);
+console.log(resultado);*/
+
+// Variable Booleanas
+/*var numUno = parseInt(prompt("Introduzca un Número"));
+var numDos = parseInt(prompt("Introduzca un Número"));
+
+var resultado = numUno == numDos;
+console.log(resultado);*/
+
+// Variable Arrays - Cadenas - Arreglos
+/*var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+var semana = [1, 2, 3, 4, 5, 6, 7];
+var valores = [1, 2, 3, 4, 5, 6, 7];
+
+console.log(meses);
+
+console.log(meses[3]);
+
+console.log(meses[5] + meses[3]);
+
+console.log(semana);
+
+console.log(semana[5] + " " + semana[3]);
+
+console.log(valores[6] + valores[8]);*/
+
+// Variable Global
+var variableGlobal = "Esto es una Variable Global";
+
+// Funciones 
+miFuncion = function() {
+	variableGlobal = "variableGlobal dentro de una funcion";
+	var variableLocal = "Esta es Local";
+	document.write(variableGlobal + "<br />" + variableLocal); 
+}
+
+otraFuncion = function() {
+	document.write(variableGlobal + "<br />" + variableLocal); 
+}
+
+miFuncion();
+otraFuncion();
+
---
+++ js/variables.js
@@ -0,0 +1,13 @@
+document.write("Practicas con variables en JS");
+
+alert("Ventana de alerta para usuario"); 
+
+// Información sobr el usuario
+var nombre = prompt("Escriba su nombre, por favor: ");
+document.write("<h1>Bienvenid@: " + nombre + "</h1><br />");
+
+var edad = prompt("Escriba su edad, por favor: ");
+document.write("Su edad es " + edad + " años<br />");
+
+var telefono = prompt("Introduzca su número de telefono: ");
+document.write("Teléfono: " + telefono);
